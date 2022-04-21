7086f2ed3e4586a8a415732a825f74fac81896ee
2020201079 <nambigarivarun@gmail.com>
2022-04-21 13:33:40 +0530

    Log files have t_id

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ cursor = connection.cursor()
 def execute_phase1(query,t_id):
     global num_commits
     print("Write prepare in log ")
-    f.write("Prepare "+query+" "+t_id+"\n")
+    f.write("Prepare \""+query+"\" "+t_id+"\n")
     f.flush()
 
     coord_ready = True
@@ -37,7 +37,7 @@ def execute_phase1(query,t_id):
 
     if coord_ready == False:
         print("Coordinator is not prepared to run the tran ")
-        f.write("Abort "+query+"\n")
+        f.write("Abort \""+query+"\" "+t_id+"\n")
         f.flush()
         connection.rollback()
         return coord_ready
@@ -58,7 +58,7 @@ def execute_phase1(query,t_id):
 def execute_phase2(query,t_id):
     global num_commits
     if(num_commits < num_of_clients):
-        f.write("Abort "+query+" "+t_id+"\n")
+        f.write("Abort \""+query+"\" "+t_id+"\n")
         f.flush()
         connection.rollback()
         print("all clients are not ready hence aborting and writing in log")
@@ -72,7 +72,7 @@ def execute_phase2(query,t_id):
             response = requests.post(curr_url, json=data)
             # client.send(("Abort "+query).encode())
     else:
-        f.write("Commit "+query+"\n")
+        f.write("Commit \""+query+"\" "+t_id+"\n")
         f.flush()
         connection.commit()
         print("committed at coord and writing in log")
@@ -87,12 +87,11 @@ def execute_phase2(query,t_id):
 
     
 def main_code():
-    # global num_commits
-
+    global num_commits
     while True:
         num_commits = 0
         query = input("Enter new query: ")
-        # query = "INSERT INTO employee_table VALUES (13,'varun','sde',27);"
+        # query = "INSERT INTO employee_table VALUES (16,'varun','sde',27);"
         t_id = "t"+str(uuid.uuid4().hex)
         coord_ready = execute_phase1(query,t_id) # phase1
         if coord_ready:
